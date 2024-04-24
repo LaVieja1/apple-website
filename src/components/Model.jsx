7295d83eb00ea27c52b2ae86230724ee1ba9972e
Santi <canosantiago404@gmail.com>
2024-04-24 10:03:38 -0300

    estructura inicial de componente model

---
+++ src/components/Model.jsx
@@ -0,0 +1,53 @@
+import gsap from "gsap";
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+
+import ModelView from "./ModelView";
+import { yellowImg } from "../utils";
+
+const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState({
+    title: "iPhone 15 Pro in Natural Titanium",
+    color: ["#8F8A81", "#ffe7b9", "#6f6c64"],
+    img: yellowImg,
+  });
+
+  // camera control for model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  // model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // rotaion
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
+  useGSAP(() => {
+    gsap.to("#heading", {
+      y: 0,
+      opacity: 1,
+    });
+  }, []);
+
+  return (
+    <section className="common-padding">
+      <div className="screen-max-width">
+        <h1 id="heading" className="section-heading">
+          Take a closer look.
+        </h1>
+
+        <div className="flex flex-col items-center mt-5">
+          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
+            <ModelView />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Model;
