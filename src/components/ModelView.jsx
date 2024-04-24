f6c1e3f859632165ef94402ef6dfc499a4470a11
Santi <canosantiago404@gmail.com>
2024-04-24 10:39:18 -0300

    modelo aparece en View

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,42 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import { Suspense } from "react";
+import { Html, PerspectiveCamera, View } from "@react-three/drei";
+
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full ${index === 2} ? 'right-[-100%] : ''`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+      <Suspense
+        fallback={
+          <Html>
+            <div>Loading</div>
+          </Html>
+        }
+      >
+        <IPhone />
+      </Suspense>
+    </View>
+  );
 };
 
 export default ModelView;
